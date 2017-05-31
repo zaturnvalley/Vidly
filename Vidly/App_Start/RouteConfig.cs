33104e87cc6cce74a77933cd3b3edc0ea77c7a0c
Zach Mason <zacharycmason@gmail.com>
2017-05-31 13:47:42 -0700

    added viewmodel, customer clsas

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -12,11 +12,9 @@ namespace Vidly
 		public static void RegisterRoutes(RouteCollection routes)
 		{
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-			routes.MapRoute(
-				"MoviesByReleaseDate",
-				"movies/released/{year}/{month}",
-				new { controller = "Movies", action = "ByReleaseDate" },
-				new { year = @"2015|2016", month = @"\d{2}" });
+
+			routes.MapMvcAttributeRoutes();
+
 			routes.MapRoute(
 				name: "Default",
 				url: "{controller}/{action}/{id}",
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -13,12 +14,19 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
 			var movie = new Movie() { Name = "The Room" };
+			var customers = new List<Customer>
+			{
+				new Customer { Name = "Customer 1"},
+				new Customer { Name = "Customer 2"}
+			};
 
-			return View(movie);
+			var viewModel = new RandomMovieViewModel
+			{
+				Movie = movie,
+				Customers = customers
+			};
+
+			return View(viewModel);
         }
-		public ActionResult ByReleaseDate(int year, int month)
-		{
-			return Content(year + "/" + month);
-		}
 	}
 }
