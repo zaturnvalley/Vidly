27489bd822bea13b502b36c98ea5a6f4749272ec
Zach Mason <zacharycmason@gmail.com>
2017-05-31 12:09:17 -0700

    custom based routing demo

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -16,18 +16,9 @@ namespace Vidly.Controllers
 
 			return View(movie);
         }
-		public ActionResult Edit(int id)
+		public ActionResult ByReleaseDate(int year, int month)
 		{
-			return Content("id=" + id);
-		}
-		// movies
-		public ActionResult Index(int? pageIndex, string sortBy)
-		{
-			if (!pageIndex.HasValue)
-				pageIndex = 1;
-			if (String.IsNullOrWhiteSpace(sortBy))
-				sortBy = "Name";
-			return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+			return Content(year + "/" + month);
 		}
 	}
 }
